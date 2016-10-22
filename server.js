61cecdfbe48b06fd4f78230a60b913073c1dd565
cgolinski <cgolinski@users.noreply.github.com>
2016-10-22 19:46:46 -0400

    save tweets to json file

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var fs = require('fs');
 var http = require('http');
+var jsonBody = require('body/json');
 var IS_AN_IMAGE_REGEX = /\.jpg$/;
 
 function postTweets(request, response) {
-	response.end('blah');
+	var encoding = 'utf-8';
+
+	fs.readFile(__dirname + '/tweets.json', encoding, function(error, fileContents) {
+		if (error) { return; }
+
+		var parsedFile = JSON.parse(fileContents);
+
+		jsonBody(request, response, function(error, bodyPayload){
+			parsedFile.data.push( { text: bodyPayload.text } );
+
+			fs.writeFile(__dirname + '/tweets.json', JSON.stringify(parsedFile), function(error) {
+				if (error) { return; }
+			
+				response.statusCode = 201;
+				response.end("Created");
+			});
+		});
+	});
 }
 
 function defaultRoute(request, response) {
@@ -19,7 +37,7 @@ function defaultRoute(request, response) {
 	}
 
 	fs.readFile(__dirname + filePath, encoding, function (error, fileContents) {
-		 response.end(fileContents);
+		response.end(fileContents);
 	});
 }
 
